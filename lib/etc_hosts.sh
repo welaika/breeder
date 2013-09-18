f2a156c9e8887fba3e296e05143006a8bb2f0a17
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-09-18 18:06:11 +0200

    refactor: reindented, cleaned main script and created new library

--- lib/etc_hosts.sh
+++ lib/etc_hosts.sh
@@ -1,11 +1,14 @@
 function add_etc_hosts_entry() {
-  local hostname=$1
+	local hostname=$1
 
-  if [[ ! $(grep ${hostname} /etc/hosts) ]]; then
-    info "Writing '${hostname}' in /etc/hosts"
-    echo "127.0.0.1 $hostname" >> /etc/hosts
-  else
-    info "'${hostname}' is already in the /etc/hosts file"
-  fi
+	if [[ ! $(grep ${hostname} /etc/hosts) ]]; then
+		info "Writing '${hostname}' in /etc/hosts"
+		echo "127.0.0.1 $hostname" >> /etc/hosts
+	else
+		info "'${hostname}' is already in the /etc/hosts file"
+	fi
 }
 
+function update_etc_hosts() {
+	add_etc_hosts_entry $site
+}
--- lib/wordless.sh
+++ lib/wordless.sh
@@ -1,11 +1,20 @@
 function wordless(){
-  if [[ ! -x `sudo -u $SUDO_USER bash -l -c "which wordless"` ]]; then
-    warning "You need wordless gem installed in order to bootstrap a new"
-    warning "WordLess project"
+	if [[ ! -x `sudo -u $SUDO_USER bash -l -c "which wordless"` ]]; then
+		warning "You need wordless gem installed in order to bootstrap a new"
+		warning "WordLess project"
 
-    exit 1
-  fi
+		exit 1
+	fi
 
-  sudo -u $SUDO_USER bash -l -c "wordless new $folder --locale=$wordless_locale"
+	pushd $folder > /dev/null
+	sudo -u $SUDO_USER bash -l -c "wordless new $folder --locale=$wordless_locale"
+	popd
 }
 
+function bootstrap_wordless() {
+	if [[ $wordless ]]; then
+		info "Bootstrapping WordLess..."
+		wordless;
+	fi
+	if_error "Error occurred"
+}
